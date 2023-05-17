ce4e4e8356e447847e0e9a756bc05edc0c81b6d0
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-17 23:28:28 +0600

    mod6task1

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,17 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
-
+public class Bird extends Animal{
+    Bird() {
+        super("blue", 2, false);
+    }
+    @Override
+    String getDescription(){
+        return super.getDescription() + " Moreover, it has 2 wings and can fly.";
+    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(new Bird().getDescription());
+//        System.out.println(new Dog().getDescription());
+//        System.out.println(new Animal("bb", 1, true).getDescription());
+//    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,12 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal{
+    Dog() {
+        super("brown", 4, true);
+    }
 
+//    @Override
+//    String getDescription() {
+//        return super.getDescription();
+//    }
 }
